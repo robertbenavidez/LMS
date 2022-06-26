3b6e2367609bca1d6ecb4de8804a938c1c4a6138
Robert Benavidez <murabitbenavidez@gmail.com>
2022-06-26 00:32:26 -0400

    dummy login and register page set up

---
+++ client/pages/register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+        <>
+            <h1 className='jumbotron bg-primary square'>Register</h1>
+            <p>Registration page</p>
+        </>
+    )
+}
+
+export default Register;
