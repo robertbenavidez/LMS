191694f440c84f25d0dc8669afb2feee3d905d3a
Robert Benavidez <murabitbenavidez@gmail.com>
2022-06-27 20:48:30 -0400

    middleware applied and first test route created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log('server setup')
+import express from 'express';
+import cors from 'cors';
+const morgan = require('morgan');
+require('dotenv').config();
+
+//create exprress app
+const app = express()
+
+// apply middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+// Routes
+app.get('/', (req, res) => {
+    // test response
+    res.send('you hit the server')
+})
+
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
