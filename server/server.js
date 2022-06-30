b878227fe7981dfd6051f8652beb5e111f49a01d
Robert Benavidez <murabitbenavidez@gmail.com>
2022-06-29 23:19:39 -0400

    mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs'
+import mongoose from 'mongoose';
 const morgan = require('morgan');
 require('dotenv').config();
 
 //create exprress app
 const app = express()
+
+// Database
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
 
 // routes
 fs.readdirSync('./routes').map((route) => 
